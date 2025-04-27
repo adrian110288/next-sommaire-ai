2334a1c65bd4e45cdae63bc9138920e797f3d138
Adrian Lesniak <adrian110288@protonmail.com>
2025-04-27 19:19:31 +0100

    Render md

--- src/components/summaries/summary-viewer.tsx
+++ src/components/summaries/summary-viewer.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
+import ReactMarkdown from "react-markdown";
 
 function SummaryViewer({ summary }: { summary: string }) {
-  return (
-    <div>
-        {summary}
-    </div>
-  )
+    return (
+        <div>
+            <ReactMarkdown>{summary}</ReactMarkdown>
+        </div>
+    );
 }
 
 export default SummaryViewer
